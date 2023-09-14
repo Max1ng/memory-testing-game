eff7dd4982df1d0a7a6190902cf731c29300daea
Max Toth <maxjtoth@gmail.com>
2023-09-13 23:23:41 -0700

    initial commit

---
+++ src/main/java/m1/App.java
@@ -0,0 +1,15 @@
+package m1;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        GUI window = new GUI();
+        window.setVisible(true);
+
+        
+
+    }
+}
